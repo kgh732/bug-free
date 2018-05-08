3b632e8ededcad9175fb72c82f8680de6fcc98b8
kgh732 <milkyan732@naver.com>
2018-05-08 09:42:06 +0900

    Init

---
+++ main.c
@@ -0,0 +1,10 @@
+
+#include "diary.h"
+
+int main()
+{
+	memo();
+	calendar();
+	return 0;
+}
+
---
+++ memo.c
@@ -0,0 +1,9 @@
+#include "diary.h"
+int memo()
+{
+	printf("function meno.\n");
+	return 0;
+	
+}
+
+
